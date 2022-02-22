03890cd626ff5779980f7c501c266a9e3775d71f
jiyoung-dev <janice10508@gmail.com>
2022-02-22 10:38:32 +0900

    add habit tracker

---
+++ src/app.jsx
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import './app.css';
+import Habits from './components/habits';
+import Navbar from './components/navbar';
+
+class App extends Component {
+  state = {
+    habits: [
+      { id: 1, name: 'Reading', count: 0 },
+      { id: 2, name: 'Running', count: 0 },
+      { id: 3, name: 'Coding', count: 0 },
+    ],
+  };
+
+  handleIncrement = habit => {
+    // const habits = [...this.state.habits];  // 상태가 update 될때마다 habits를 직접수정하는 방식 
+    // const index = habits.indexOf(habit);
+    // habits[index].count++;
+    const habits = this.state.habits.map(item => {  // 기존의 state.habits을 돌면서 id가 같은경우, 새로운 오브젝트 habits를 만든다 
+      if (item.id === habit.id) {
+        return { ...habit, count: habit.count + 1 };
+      }
+      return item;
+    });
+    this.setState({ habits });
+  };
+
+  handleDecrement = habit => {
+    // const habits = [...this.state.habits];
+    // const index = habits.indexOf(habit);
+    // const count = habits[index].count - 1;
+    // habits[index].count = count < 0 ? 0 : count;
+    const habits = this.state.habits.map(item => {
+      if (item.id === habit.id) {
+        const count = habit.count - 1;
+        return { ...habit, count: count < 0 ? 0 : count };
+      }
+      return item;
+    });
+    this.setState({ habits });
+  };
+
+  handleDelete = habit => {
+    const habits = this.state.habits.filter(item => item.id !== habit.id);
+    this.setState({ habits });
+  };
+
+  handleAdd = name => {
+    const habits = [...this.state.habits, { id: Date.now(), name, count: 0 }];  //고유한 id값을 주기위해 Date클래스 사용. name: name 줄여서 name 가능
+    this.setState({ habits });
+  };
+
+  handleReset = () => {
+    const habits = this.state.habits.map(habit => {
+      return { ...habit, count: 0 };
+    });
+    this.setState({ habits });
+  };
+
+  render() {
+    return (
+      <>
+        <Navbar
+          totalCount={this.state.habits.filter(item => item.count > 0).length}
+        />
+        <Habits
+          habits={this.state.habits}
+          onIncrement={this.handleIncrement}
+          onDecrement={this.handleDecrement}
+          onDelete={this.handleDelete}
+          onAdd={this.handleAdd}
+          onReset={this.handleReset}
+        />
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/habits.jsx
@@ -0,0 +1,36 @@
+import React, { PureComponent } from 'react';
+import Habit from './habit';
+import HabitAddForm from './habitAddForm';
+
+class Habits extends PureComponent {
+
+  handleAdd = name => {
+    this.props.onAdd(name);
+  }
+
+  render() {
+    console.log("habits");
+    return (
+      <>
+        <HabitAddForm 
+          onAdd={this.handleAdd}
+        />
+        <ul>
+          {this.props.habits.map(habit => (
+            <Habit
+              key={habit.id}
+              habit={habit}
+              onIncrement={this.props.onIncrement}
+              onDecrement={this.props.onDecrement}
+              onDelete={this.props.onDelete}
+            />
+          ))}
+        </ul>
+        <button className="habits-reset" onClick={this.props.onReset}>Reset All</button>
+      </>
+        
+    );
+  }
+}
+
+export default Habits;
