cb91167f358a9c61b7779f08837f7a373d00089b
jiyoung-dev <janice10508@gmail.com>
2022-02-22 11:14:11 +0900

    Updates

---
+++ precache-manifest.8f51cab3bf9a34bb4a3e2c019ffcfe6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bd7f83b194d7d85c47af670be93d7c5",
+    "url": "/react_basic_habit_tracker/index.html"
+  },
+  {
+    "revision": "e26b5d9d0e75d0269bbe",
+    "url": "/react_basic_habit_tracker/static/css/main.fe771ccb.chunk.css"
+  },
+  {
+    "revision": "30ea2229acc1adba22ad",
+    "url": "/react_basic_habit_tracker/static/js/2.e8da4bea.chunk.js"
+  },
+  {
+    "revision": "1ea337ab0f6eaf802a045a4531cad9ed",
+    "url": "/react_basic_habit_tracker/static/js/2.e8da4bea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e26b5d9d0e75d0269bbe",
+    "url": "/react_basic_habit_tracker/static/js/main.a4ac5ec9.chunk.js"
+  },
+  {
+    "revision": "cce1584d664dd6fcc7a5",
+    "url": "/react_basic_habit_tracker/static/js/runtime-main.929c0c59.js"
+  }
+]);
